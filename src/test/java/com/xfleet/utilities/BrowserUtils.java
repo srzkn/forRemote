5c454325e8c323f94d3c41d1039a3633127a1b13
Tuğrahan Sezer <tunasezer123@gmail.com>
2023-03-21 10:48:47 +0300

    Added browser utils with metods

--- src/test/java/com/xfleet/utilities/BrowserUtils.java
+++ src/test/java/com/xfleet/utilities/BrowserUtils.java
@@ -32,7 +32,7 @@ public class BrowserUtils {
     }
 
     /*
-    This method accepts 3 arguments.
+    This method accepts  3 arguments.
     Arg1: webdriver
     Arg2: expectedInUrl : for verify if the url contains given String.
         - If condition matches, will break loop.
