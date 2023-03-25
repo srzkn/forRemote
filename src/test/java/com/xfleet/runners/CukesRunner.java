18ac0f094bba1cc64334a91cfe480b13c43072b8
Tuğrahan Sezer <tunasezer123@gmail.com>
2023-03-25 21:06:05 +0300

    changed some of the codes

--- src/test/java/com/xfleet/runners/CukesRunner.java
+++ src/test/java/com/xfleet/runners/CukesRunner.java
@@ -18,7 +18,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/xfleet/step_definitions",
         dryRun = false,
-        tags = "@XFLEET10-705",
+        tags = "",
         publish = true
 )
 public class CukesRunner {
